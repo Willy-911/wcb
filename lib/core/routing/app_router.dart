8959c417ffda15c90212846e751416d1f0046a96
Youssef Hesham William <youssefhesham1811@gmail.com>
2026-02-24 17:48:34 +0200

    - Added images and svgs folders for assets. - Added flutter_svg & added flutter_screenutil. - Made a file for doc app client. - Added navigation extension. - Added routing folder that contains ruler class and rulers file. - Added colors & text styles. - Added empty login screen. - Added onboarding screen.

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:wcb/core/routing/routes.dart';
+import 'package:wcb/features/login/ui/login_screen.dart';
+import 'package:wcb/features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+        case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/doc_app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:wcb/core/routing/app_router.dart';
+import 'package:wcb/core/routing/routes.dart';
+import 'package:wcb/core/theming/colors.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white
+        ),
+        debugShowMaterialGrid: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
